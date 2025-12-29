93efc084d399200dc5581a28e8f78048544e985c
Orsell <34631691+OrsellGit@users.noreply.github.com>
2025-12-29 01:47:11 -0700

    feat: Cleaned up the run scripts for running the Wiki. Wiki software submodule should now also get properly updated when new changes occur.

--- run.bat
+++ run.bat
@@ -1,23 +1,26 @@
 @ECHO OFF
 REM Basic getting started script for the Wiki generator
 
-REM Ensure we have the generator
-IF NOT EXIST site\.git (
-	echo Cloning submodule site...
-	git submodule init site
-	git submodule update site
+REM Ensure the wiki software exists and is update to date.
+IF NOT EXIST "site/.git" (
+	echo "Cloning wiki source submodule as 'site'..."
+	git submodule update --init site
+) ELSE (
+	REM Pull new strata-wiki changes if the repository gets updated.
+	echo "Pulling any new wiki source submodule changes..."
+	git submodule update --remote
 )
 
 cd site
 
-REM Ensure we have our dependencies
+REM Make sure the node modules exist for the Wiki.
 IF NOT EXIST node_modules (
-	echo Installing modules...
+	echo "Installing node modules..."
 	call npm i
 )
 
 REM Ready to rock and roll!
-echo Starting up...
+echo "Starting up..."
 call npm run build
 call npm run dev
 
